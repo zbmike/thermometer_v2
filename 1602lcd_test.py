682874af890dcaeddfd6ffc02d1c75848d0828ce
Michael He <zh2312@columbia.edu>
2021-04-07 20:19:20 -0400

    Add testing code for 16x2 lcd screen

---
+++ 1602lcd_test.py
@@ -0,0 +1,13 @@
+from RPLCD.gpio import CharLCD
+from RPi import GPIO
+from time import sleep
+
+lcd = CharLCD(cols=16, rows=2, pin_rs=15, pin_e=16, pins_data=[21, 22, 23, 24],
+              numbering_mode=GPIO.BOARD)
+
+lcd.cursor_pos = (0, 0)
+lcd.write_string("hello")
+lcd.cursor_pos = (1, 0)
+lcd.write_string("world")
+# lcd.clear()
+# lcd.close(clear = True)
